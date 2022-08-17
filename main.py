cce7193506c71e1d23e53581cafeb4870e78d16f
ejmaza88 <esvin@fimed.com>
2022-08-17 17:19:43 -0500

    Part two, include code refactoring

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ from typing import List, Union
 PUZZLE_INPUT = "puzzle.txt"
 
 CHUNK_KEYS = {"(": ")", "[": "]", "{": "}", "<": ">"}
-POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+SYNTAX_POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+VALID_POINTS = {")": 1, "]": 2, "}": 3, ">": 4}
+SCORE_CONSTANT = 5
 
-openers = CHUNK_KEYS.keys()
-closers = CHUNK_KEYS.values()
+OPENERS = CHUNK_KEYS.keys()
+CLOSERS = CHUNK_KEYS.values()
 
 
-def illegal_chunk(chunk: str = None, invalid_list: List[str] = None) -> None:
+def process_chunk(chunk: str = None) -> Union[List, str]:
     char_list = []
 
     for char in chunk:
-        if char in openers:
+        if char in OPENERS:
             char_list.append(char)
 
-        elif char in closers:
+        elif char in CLOSERS:
             last_char_from_list = char_list[-1]
             closer_char = CHUNK_KEYS.get(last_char_from_list)
 
@@ -25,28 +27,69 @@ def illegal_chunk(chunk: str = None, invalid_list: List[str] = None) -> None:
                 char_list = char_list[:-1]
 
             else:
-                invalid_list.append(char)
-                return
+                return char
+
+    return char_list
 
 
-def syntax_error_score(invalid_list: List[str] = None) -> int:
+def calculate_syntax_error_score(invalid_list: List[str] = None) -> int:
     score = 0
+
     for char in invalid_list:
-        score += POINTS.get(char)
+        score += SYNTAX_POINTS.get(char)
 
     return score
 
 
-if __name__ == "__main__":
+def autocomplete_score_per_chunk(valid_chunk: List[str] = None) -> int:
+    score = 0
+
+    for index, _ in enumerate(valid_chunk, start=1):
+        char = valid_chunk[-index]
+        closer_char = CHUNK_KEYS.get(char)
+        score = (score * SCORE_CONSTANT) + VALID_POINTS.get(closer_char)
+
+    return score
+
+
+def find_autocomplete_score(score_list: List[int] = None) -> int:
+    return sorted(score_list)[int(len(score_list) / 2)]
+
+
+def syntax_scoring_part_one() -> None:
     invalid = []  # list of invalid chars
 
     with open(PUZZLE_INPUT, "r") as f:
         for line in f:
-            illegal_chunk(chunk=line.strip(), invalid_list=invalid)
+            chunk_output = process_chunk(chunk=line.strip())
+            if isinstance(chunk_output, str):
+                invalid.append(chunk_output)
 
-    error_score = syntax_error_score(invalid_list=invalid)
+    error_score = calculate_syntax_error_score(invalid_list=invalid)
 
-    # print(invalid)
     print(error_score)
+
+
+def syntax_scoring_part_two() -> None:
+    score_list = []
+
+    with open(PUZZLE_INPUT, "r") as f:
+        for line in f:
+            chunk_output = process_chunk(chunk=line.strip())
+
+            if isinstance(chunk_output, list):
+                score = autocomplete_score_per_chunk(valid_chunk=chunk_output)
+                score_list.append(score)
+
+    autocomplete_score = find_autocomplete_score(score_list=score_list)
+    print(autocomplete_score)
+
+
+if __name__ == "__main__":
+    print("-- Part One --")
+    syntax_scoring_part_one()
+    print()
+    print("-- Part Two --")
+    syntax_scoring_part_two()
 
 
